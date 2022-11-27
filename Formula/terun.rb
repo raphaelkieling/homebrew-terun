33c96ccddffeb33a00f79fcdbb3dfebb1051d1c2
raphael.kieling <raphael.kieling@telus.com>
2022-11-27 15:34:11 -0300

    feat: new version

--- Formula/terun.rb
+++ Formula/terun.rb
@@ -1,9 +1,9 @@
 class Terun < Formula
     desc "Tool to create files based on templates"
     homepage "https://github.com/raphaelkieling/terun-go"
-    url "https://github.com/raphaelkieling/terun-go/releases/download/0.0.2/terun-0.0.2-darwin-amd64.tar.gz"
-    version "0.0.2"
-    sha256 "f5e41ed0665e7784d148b666e8800e6978ef06a9046320557a7e9060c27aff7e"
+    url "https://github.com/raphaelkieling/terun-go/releases/download/0.0.3/terun-0.0.3-darwin-amd64.tar.gz"
+    version "0.0.3"
+    sha256 "173cea6926db92b6350c03af76c06ebe74ee591774d3c2400cfba111a358ca09"
   
     def install
         bin.install "terun"
