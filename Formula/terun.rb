78874fed3238d57c851cc06a5e5580c1c38884b1
raphael.kieling <raphael.kieling@telus.com>
2022-11-27 14:54:24 -0300

    first commit

---
+++ Formula/terun.rb
@@ -0,0 +1,16 @@
+class Terun < Formula
+    desc "Tool to create files based on templates"
+    homepage "https://github.com/raphaelkieling/terun-go"
+    url "https://github.com/raphaelkieling/terun-go/releases/download/0.0.1/terun-go-0.0.1-darwin-amd64.tar.gz"
+    version "0.0.1"
+    sha256 "7fe45dae52e7f366b3014d77605f9b6fd1294b01ff73acdc0c18d8072e74f618"
+  
+    def install
+      bin.install "terun"
+    end
+  
+    test do
+      system bin/"terun", "version"
+    end
+  end
+  
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Terun
+
+## Prerequisites
+
+You need to have [Homebrew](https://brew.sh/) installed on your system.
+
+## Installing
+
+Install the Terun with
+
+```bash
+brew install ??
+```
+
+## Update
+
+1. Update link and version in `Formula/terun.rb`
+2. Run `brew fetch --force Formula/terun.rb` to determine the new checksum
+3. Update checksum
